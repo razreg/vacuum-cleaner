f9f4050b3daeb0b3816d04a2b3842933fde933e3
idoshvartz <idoshvartz@mail.tau.ac.il>
2016-03-13 16:28:40 +0200

    ido's first commit after the weekend

--- vacuum-cleaner/vacuum-cleaner/House.h
+++ vacuum-cleaner/vacuum-cleaner/House.h
@@ -1,7 +1,9 @@
 #pragma once
 #include "Position.h"
+#include <iostream>
 #include <string>
 #include <vector>
+#include <fstream>
 
 using namespace std;
 
@@ -29,8 +31,32 @@ public:
 		}
 	}
 
+	string getShortName(){
+		return this->shortName;
+	}
+
+	string getDescription(){
+		return this->description;
+	}
+
+	int getNumRows(){
+		return this->numRows;
+	}
+
+	int getnumCols(){
+		return this->numCols;
+	}
+
+	vector<vector<char>>* getMatrix(){
+		return this->matrix;
+	}
+
 	int getDirtLevel(Position& position) const {
 		char value = (*matrix)[position.X][position.Y];
+		if (value == ' '){ 
+			//space is equal to 0
+			return 0;
+		}
 		if (value < '0' || value > '9') {
 			// TODO handle illegal position (wall, docking, outside the house, etc.) - throw exception?
 		}
@@ -42,6 +68,21 @@ public:
 		return toupper((*matrix)[position.X][position.Y]) == WALL;
 	}
 
-	static House& deseriallize(const string& filePath);
+	Position getDockingStation(){
+		int i, j;
+		Position pos = { 0, 0 };
+		for (i = 0; i < numRows; i++){
+			for (j = 0; j < numCols; j++){
+				if ((*matrix)[i][j] == 'D'){
+					pos = { i, j };
+					return pos;
+				}
+					
+			}
+		}
 
+		//TODO return error message? exit?
+	}
+
+	static House& deseriallize(const string& filePath);
 };
