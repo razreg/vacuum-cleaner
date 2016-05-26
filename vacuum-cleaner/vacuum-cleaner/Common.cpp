0fe3995512b14cf0b4cf27a113aa883360f9767e
Raz Regev <razregev@gmail.com>
2016-05-26 12:57:31 +0300

    fixing trimString function

--- vacuum-cleaner/vacuum-cleaner/Common.cpp
+++ vacuum-cleaner/vacuum-cleaner/Common.cpp
@@ -41,11 +41,11 @@ void trimString(string& str) {
 	if (str.length() == 0) {
 		return;
 	}
-	size_t first = max(str.find_first_not_of(' '), str.find_first_not_of('\t'));
-	char lasts[] = { ' ', '\t', '\r', '\n', '\0' };
-	size_t last = str.length() - 1;
-	for (int i = 0; lasts[i] != '\0'; ++i) {
-		last = min(last, str.find_last_not_of(lasts[i]));
+	string whitespaces(" \t\f\v\n\r");
+	size_t first = str.find_first_not_of(whitespaces);
+	if (first == string::npos) {
+		first = str.length();
 	}
+	size_t last = str.find_last_not_of(whitespaces);
 	str = str.substr(first, (last - first + 1));
 }
